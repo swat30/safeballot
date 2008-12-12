3bba17f8258622c7c5f838df96a5010b8de1b935
Jacob Godin <jacobgodin@gmail.com>
2008-12-12 14:39:45 -0400

    Added emailing functionality

--- modules/Campaigns/scripts/autosender.php
+++ modules/Campaigns/scripts/autosender.php
@@ -1,3 +1,39 @@
 <?php
-var_dump($_SERVER);
+//if($_SERVER['HOSTNAME'] == 'woz.norex.ca' && $_SERVER['LOGNAME'] == 'safeballots'){
+	ini_set('safe_mode', 'off');
+	include(getcwd().'/core/Database.php');
+	include(getcwd().'/modules/Campaigns/include/Campaign.php');
+	include(getcwd().'/modules/Campaigns/include/CampaignUser.php');
+	$sql = 'SELECT id FROM campaigns where autosend = 1';
+	if(@$argv[1] == 'debug'){
+		$debug = true;
+	}
+	$results = Database::singleton()->query_fetch_all($sql);
+	foreach($results as &$campaign){
+		$campaign = new Campaign($campaign['id']);
+		switch ($campaign->calcStatus(true)){
+			case 2:
+				break;
+			case 1:
+				$sql = 'SELECT aut_email FROM auth WHERE aut_agp_id = '.$campaign->getGroup().' LIMIT 1';
+				$email = Database::singleton()->query_fetch($sql);
+				$email = $email['aut_email'];
+				$sql = 'SELECT agp_name FROM auth_groups WHERE agp_id = '.$campaign->getGroup();
+				$group = Database::singleton()->query_fetch($sql);
+				$group = $group['agp_name'];
+				$campaign->mailOut('votes', $group, $email);
+				if($debug){
+					echo "Sent ".$campaign->getName()." emails.\n";
+				}
+			default:
+				$sql = 'UPDATE campaigns SET autosend = 0 WHERE id = '.$campaign->getId();
+				$result = Database::singleton()->query($sql);
+				if($debug){
+					echo "Removed ".$campaign->getName()."'s autosend.\n";
+				}
+				break;
+		}
+	}
+//}
+
 ?>
