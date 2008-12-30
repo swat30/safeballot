0b2308be1bd555f8e529a840b726a78479f9545a
Jacob Godin <jacobgodin@gmail.com>
2008-12-30 14:05:34 -0400

    IE test

--- js/admin.js
+++ js/admin.js
@@ -142,9 +142,7 @@ var NorexUI = Class.create(Facebox, {
 			},
 			onComplete: function(transport) {
 				if (!transport.responseText.match(/class="error/)) {
-					alert('before message');
 					//new Message({message: 'Item was updated successfully'});
-					alert('after');
 					ui.close();
 				} else {
 					//new Message({type: 'error', message: 'Not all fields were filled in'});
