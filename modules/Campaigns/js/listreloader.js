24f9aca308d2fdc3075de65d774d636ac33fb0e2
Jacob Godin <jacobgodin@gmail.com>
2009-02-27 13:16:06 -0400

    Listreloader js change

--- modules/Campaigns/js/listreloader.js
+++ modules/Campaigns/js/listreloader.js
@@ -2,7 +2,7 @@ Event.observe(window, 'load', function(event){
 	var interval = 60;
 	var refresher = function() {
 		new Ajax.PeriodicalUpdater('module_content', '/admin/Campaigns', {
-			method: 'get',
+			method: 'post',
 			frequency: interval,
 			decay: 1
 		});
